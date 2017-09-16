3eb7ee29b79acf431b633cb7e84e101054765dbc
judesahayaraj <judepaul.arokiaraj@gmail.com>
2017-09-16 17:39:32 +0530

    Refined the Questionaire UI

--- find.php
+++ find.php
@@ -5,10 +5,10 @@
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
       <?php
 
-      require("connect.php");
+      // require("connect.php");
       date_default_timezone_set("Asia/Kolkata");
       $createdate= date('Y-m-d H:i:s');
-      echo mysqli_error($link);
+      // echo mysqli_error($link);
 
       ?>
       <style type="text/css">
@@ -63,27 +63,121 @@
   </div>
 </nav>
  <form method = "POST">
- <div id = "new_group">
-    <p>Name of the startup: <input type = "text" name = "title"></p>
-    <p>What sort of developer are you looking for :<select>
-      <option value = "web">Web developer</option>
-      <option value = "app">App developer</option>
-    </select></p>
-    <p>Description of the idea (within 50 words)</p> <p><textarea rows = "5" cols = "50"></textarea></p>
-    <p>Time requirement:    <input type = "text"></p>
-    <p>Services necessary:  <select name = "time">
-                  <option value = "one">UI/UX</option>
-                  <option value = "three">Mockup/Wireframe</option>  
-                  <option value = "month">Testing</option>
-                  <option value = "two">Security</option>
-                  <option value = "twelve">Entire stack</option>
-                  <option value = "none">Others</option>
-                </select></p>
-    <p> Additional Requirements(if any)</p><p><input type = "text"></p>
-    
-
-    <input type = "submit" name = "find" value = "Find developer" id = "submit">
-
+ <div class="row">
+ 	<div class="form-group">
+		<div class="col-sm-12 col-md-12 col-lg-2"></div>
+ 		<div class="col-sm-12 col-md-12 col-lg-2">
+		</div>
+		<div class="col-sm-12 col-md-12 col-lg-4 text-center">
+    		<span style="font-size:36px; font-weight:bold;">Questionaire</span><br>
+		</div>
+	</div>
+</div>
+<div class="row"><br/></div>
+ <div class="row">
+ 	<div class="form-group">
+		<div class="col-sm-12 col-md-12 col-lg-2"></div>
+ 		<div class="col-sm-12 col-md-12 col-lg-2">
+			<label for="company">Name of your Startup</label>
+		</div>
+		<div class="col-sm-12 col-md-12 col-lg-4">
+    		<input type="text" name="company_name", class="form-control", required="true"><br>
+		</div>
+	</div>
+</div>
+ <div class="row">
+ 	<div class="form-group">
+		<div class="col-sm-12 col-md-12 col-lg-2"></div>
+ 		<div class="col-sm-12 col-md-12 col-lg-2">
+			<label for="company">Developer's looking for</label>
+		</div>
+		<div class="col-sm-12 col-md-12 col-lg-4">
+			 <select name="area_of_development" class="form-control">
+			  <option value="web">Web Development</option>
+			  <option value="app">App Development</option>
+			</select>
+		</div>
+	</div>
+</div>
+<div class="row"><br/></div>
+ <div class="row">
+ 	<div class="form-group">
+		<div class="col-sm-12 col-md-12 col-lg-2"></div>
+ 		<div class="col-sm-12 col-md-12 col-lg-2">
+			<label for="company">Specialisation</label>
+		</div>
+		<div class="col-sm-12 col-md-12 col-lg-4">
+			 <select name="specialisation" class="form-control">
+			  <option value="ui">UI/UX</option>
+			  <option value="mock">Mockup/Wireframe</option>
+			  <option value="test">Testing</option>
+			  <option value="devops">Devops</option>
+			  <option value="full">Full stack</option>
+			  <option value="others">Others</option>
+			</select>
+		</div>
+	</div>
+</div>
+<div class="row"><br/></div>
+ <div class="row">
+ 	<div class="form-group">
+		<div class="col-sm-12 col-md-12 col-lg-2"></div>
+ 		<div class="col-sm-12 col-md-12 col-lg-2">
+			<label for="company">Duration</label>
+		</div>
+		<div class="col-sm-12 col-md-12 col-lg-4">
+    		<input type="text" name="duration", class="form-control", required="true">
+		</div>
+	</div>
+</div>
+<div class="row"><br/></div>
+ <div class="row">
+ 	<div class="form-group">
+		<div class="col-sm-12 col-md-12 col-lg-2"></div>
+ 		<div class="col-sm-12 col-md-12 col-lg-2">
+			<label for="concept_desc">What's your Idea?<br/>(within 500 words)</label>
+		</div>
+		<div class="col-sm-12 col-md-12 col-lg-4">
+    		<textarea class="form-control" id="concept_textarea" rows="5"></textarea>
+		</div>
+	</div>
+</div>
+<div class="row"><br/></div>
+ <div class="row">
+ 	<div class="form-group">
+		<div class="col-sm-12 col-md-12 col-lg-2"></div>
+ 		<div class="col-sm-12 col-md-12 col-lg-2">
+			<label for="concept_desc">Additional Requirement<br/></label>
+		</div>
+		<div class="col-sm-12 col-md-12 col-lg-4">
+    		<textarea class="form-control" id="concept_textarea" rows="4"></textarea>
+		</div>
+	</div>
+</div>
+<div class="row"><br/></div>
+ <div class="row">
+ 	<div class="form-group">
+		<div class="col-sm-12 col-md-12 col-lg-2"></div>
+ 		<div class="col-sm-12 col-md-12 col-lg-2">
+			<label for="concept_desc">Upload Documents(if any)</label>
+		</div>
+		<div class="col-sm-12 col-md-12 col-lg-4">
+		    <span class="btn btn-default btn-file">
+		        Browse <input type="file">
+		    </span>
+		</div>
+	</div>
+</div>
+<div class="row"><br/></div>
+ <div class="row">
+ 	<div class="form-group">
+		<div class="col-sm-12 col-md-12 col-lg-2"></div>
+ 		<div class="col-sm-12 col-md-12 col-lg-2"></div>
+		<div class="col-sm-12 col-md-12 col-lg-4">
+		    <button type="button" class="btn btn-primary">Submit</button>
+		</div>
+	</div>
+</div>
     
 
   </div>
@@ -100,3 +194,25 @@
 
 </body>
 </html>
+
+<style>
+    .btn-file {
+        position: relative;
+        overflow: hidden;
+    }
+    .btn-file input[type=file] {
+        position: absolute;
+        top: 0;
+        right: 0;
+        min-width: 100%;
+        min-height: 100%;
+        font-size: 100px;
+        text-align: right;
+        filter: alpha(opacity=0);
+        opacity: 0;
+        outline: none;
+        background: white;
+        cursor: inherit;
+        display: block;
+    }
+	</style>
