52763f76fa85c6c0b3d1475539bd4a596cef7936
judesahayaraj <judepaul.arokiaraj@gmail.com>
2017-09-17 12:02:46 +0530

    Updated home page content

--- landing.php
+++ landing.php
@@ -3,17 +3,8 @@
 	<title>Health forum</title>
 	  <script src='https://www.google.com/recaptcha/api.js'></script>
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-      <?php
-      session_start();
-      ?>
-      <style type="text/css">
-  
-  
-
-
-
-
-
+       <style type="text/css">
+ 
 
       </style>
 </head>
@@ -26,7 +17,7 @@
     </div>
     <ul class="nav navbar-nav">
       <li><a href="landing.php">Home</a></li>
-      <li><a href = "profile.php">Profiles</a></li>
+      <li><a href = "#">Profiles</a></li>
       <li><a href="find.php">Find my developer</a></li>
   
     </ul>
@@ -38,9 +29,25 @@
   </div>
 </nav>
 <div id = "about">
-  <div class = 'page_header'><div class = 'jumbotron' ><h1>About us</h1></div></div>
+  <div class = 'page_header' style="padding: 10px 0 0 30px;"><h1>
+	Easily find quality Developers
+	</h1>
+	<p style="font-size:16px;">On Tech-Helpline you'll find a range of top talent, from programmers to designers, writers, customer support reps, and more.</p>
+</div>
 
-
+  <div class = 'page_header' style="padding: 30px 0 0 30px;"><h1>
+	How it Works?
+	</h1>
+	<br/>
+	<ul style="font-size: 20px;">
+		<li>Understand the customer's need using a simple questionnaire</li>
+		<br/>
+		<li>Suggest the most suitable developer for the project</li>
+		<br/>
+		<li>Following that the start-up founder has a 3 day window where we can facilitate
+and organise live chats, video calls between the customer and developer</li>
+	</ul>
+</div>
 
 </div>
 
